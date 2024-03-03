4f1fe5093b7b7488c673e3e9b6516d0ac1a8e5a8
IshratMishu <ishratjahan0425@gmail.com>
2024-03-03 16:03:14 +0600

    API added in discuss section

--- scripts/retro.js
+++ scripts/retro.js
@@ -1,7 +1,73 @@
+
 // Search & Let's Discuss Section Functionality
+const discussionPosts = async() => {
+    const response = await fetch('https://openapi.programming-hero.com/api/retro-forum/posts');
+    const data = await response.json();
+    const allData = data.posts;
 
+    const discussionPosts = document.getElementById('discussion-posts');
+    allData.forEach((item) => {
+        const div = document.createElement('div');
+        
+        div.innerHTML = `<div class="card card-side bg-base-100 shadow-xl flex flex-col lg:flex-row">
+        <div class="indicator mt-10 lg:ml-5 mx-auto">
+          <span class="indicator-item badge badge-success"></span>
+          <div class="grid w-32 lg:w-20 h-20 bg-base-300 place-items-center"><img src="images/logo.png" alt=""></div>
+        </div>
+        <div class="card-body p-1 lg:p-10">
+          <div class="flex gap-3 lg:gap-10 font-medium text-sm text-color-gray80">
+            <h5>#Music</h5>
+            <h5>Author : Awlad Hossain</h5>
+          </div>
+          <h2 class="font-bold text-black text-xl">New movie is released!</h2>
+          <p class="text-color-gray80 text-sm">Click the button to watch on Jetflix app.</p>
+          <hr class="color-black border-t-2 border-dotted">
 
+          <div class="flex justify-between">
+            <div class="flex gap-4">
+              <div class="flex items-center gap-2">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-4 h-4">
+                  <path stroke-linecap="round" stroke-linejoin="round" d="M7.5 8.25h9m-9 3H12m-9.75 1.51c0 1.6 1.123 2.994 2.707 3.227 1.129.166 2.27.293 3.423.379.35.026.67.21.865.501L12 21l2.755-4.133a1.14 1.14 0 0 1 .865-.501 48.172 48.172 0 0 0 3.423-.379c1.584-.233 2.707-1.626 2.707-3.228V6.741c0-1.602-1.123-2.995-2.707-3.228A48.394 48.394 0 0 0 12 3c-2.392 0-4.744.175-7.043.513C3.373 3.746 2.25 5.14 2.25 6.741v6.018Z" />
+                </svg>
+                <p class="text-color-gray60">560</p>
+              </div>
 
+              <div class="flex items-center gap-2">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-4 h-4">
+                  <path stroke-linecap="round" stroke-linejoin="round" d="M2.036 12.322a1.012 1.012 0 0 1 0-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178Z" />
+                  <path stroke-linecap="round" stroke-linejoin="round" d="M15 12a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
+                </svg>
+               <p class="text-color-gray60">1,568</p> 
+              </div>
+
+              <div class="flex items-center gap-2">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-4 h-4">
+                  <path stroke-linecap="round" stroke-linejoin="round" d="M12 6v6h4.5m4.5 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
+                </svg>
+               <p class="text-color-gray60">5 min</p> 
+              </div>
+            </div>
+
+          <div>
+            <button class="btn btn-circle bg-success">
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="white" class="w-6 h-6 bg-success rounded-2xl p-0.5">
+                <path stroke-linecap="round" stroke-linejoin="round" d="M21.75 9v.906a2.25 2.25 0 0 1-1.183 1.981l-6.478 3.488M2.25 9v.906a2.25 2.25 0 0 0 1.183 1.981l6.478 3.488m8.839 2.51-4.66-2.51m0 0-1.023-.55a2.25 2.25 0 0 0-2.134 0l-1.022.55m0 0-4.661 2.51m16.5 1.615a2.25 2.25 0 0 1-2.25 2.25h-15a2.25 2.25 0 0 1-2.25-2.25V8.844a2.25 2.25 0 0 1 1.183-1.981l7.5-4.039a2.25 2.25 0 0 1 2.134 0l7.5 4.039a2.25 2.25 0 0 1 1.183 1.98V19.5Z" />
+              </svg>
+            </button>
+          </div>
+          </div>
+
+        </div>
+      </div>`
+
+    
+      discussionPosts.appendChild(div);
+        
+    });
+    
+}
+
+discussionPosts();
 
 
 
