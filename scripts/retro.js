f0c3f55e99969b571abe5e34fd4c17b5d55f2d9e
IshratMishu <ishratjahan0425@gmail.com>
2024-03-03 16:58:17 +0600

    isActive color added

--- scripts/retro.js
+++ scripts/retro.js
@@ -8,19 +8,18 @@ const discussionPosts = async() => {
     const discussionPosts = document.getElementById('discussion-posts');
     allData.forEach((item) => {
         const div = document.createElement('div');
-        
         div.innerHTML = `<div class="card card-side bg-base-100 shadow-xl flex flex-col lg:flex-row">
         <div class="indicator mt-10 lg:ml-5 mx-auto">
-          <span class="indicator-item badge badge-success"></span>
-          <div class="grid w-32 lg:w-20 h-20 bg-base-300 place-items-center"><img src="images/logo.png" alt=""></div>
+          <span id="active-test" class="indicator-item badge ${item.isActive ? 'badge-success' : 'badge-error'}"></span>
+          <div class="grid w-20 h-20 bg-base-300 place-items-center"><img src="${item.image}" alt=""></div>
         </div>
         <div class="card-body p-1 lg:p-10">
           <div class="flex gap-3 lg:gap-10 font-medium text-sm text-color-gray80">
-            <h5>#Music</h5>
-            <h5>Author : Awlad Hossain</h5>
+            <h5>#${item.category}</h5>
+            <h5>Author : ${item.author.name}</h5>
           </div>
-          <h2 class="font-bold text-black text-xl">New movie is released!</h2>
-          <p class="text-color-gray80 text-sm">Click the button to watch on Jetflix app.</p>
+          <h2 class="font-bold text-black text-xl">${item.title}</h2>
+          <p class="text-color-gray80 text-sm">${item.description}</p>
           <hr class="color-black border-t-2 border-dotted">
 
           <div class="flex justify-between">
@@ -29,7 +28,7 @@ const discussionPosts = async() => {
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-4 h-4">
                   <path stroke-linecap="round" stroke-linejoin="round" d="M7.5 8.25h9m-9 3H12m-9.75 1.51c0 1.6 1.123 2.994 2.707 3.227 1.129.166 2.27.293 3.423.379.35.026.67.21.865.501L12 21l2.755-4.133a1.14 1.14 0 0 1 .865-.501 48.172 48.172 0 0 0 3.423-.379c1.584-.233 2.707-1.626 2.707-3.228V6.741c0-1.602-1.123-2.995-2.707-3.228A48.394 48.394 0 0 0 12 3c-2.392 0-4.744.175-7.043.513C3.373 3.746 2.25 5.14 2.25 6.741v6.018Z" />
                 </svg>
-                <p class="text-color-gray60">560</p>
+                <p class="text-color-gray60">${item.comment_count}</p>
               </div>
 
               <div class="flex items-center gap-2">
@@ -37,14 +36,14 @@ const discussionPosts = async() => {
                   <path stroke-linecap="round" stroke-linejoin="round" d="M2.036 12.322a1.012 1.012 0 0 1 0-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178Z" />
                   <path stroke-linecap="round" stroke-linejoin="round" d="M15 12a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
                 </svg>
-               <p class="text-color-gray60">1,568</p> 
+               <p class="text-color-gray60">${item.view_count}</p> 
               </div>
 
               <div class="flex items-center gap-2">
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-4 h-4">
                   <path stroke-linecap="round" stroke-linejoin="round" d="M12 6v6h4.5m4.5 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
                 </svg>
-               <p class="text-color-gray60">5 min</p> 
+               <p class="text-color-gray60">${item.posted_time}</p> 
               </div>
             </div>
 
@@ -56,13 +55,12 @@ const discussionPosts = async() => {
             </button>
           </div>
           </div>
-
         </div>
       </div>`
-
-    
+      
+    console.log(item);
       discussionPosts.appendChild(div);
-        
+      
     });
     
 }
